98aec6c5c4c9e14de54b642c77ba26f9143acdda
Jonathanmg1415 <jonathang1415@gmail.com>
2023-04-25 16:57:41 -0500

    Funcional

--- app/src/test/java/com/cursokotlin/mvvmexample/domain/GetQuotesUseCaseTest.kt
+++ app/src/test/java/com/cursokotlin/mvvmexample/domain/GetQuotesUseCaseTest.kt
@@ -22,32 +22,14 @@ class GetQuotesUseCaseTest {
         MockKAnnotations.init(this)
         getQuotesUseCase = GetQuotesUseCase(quoteRepository)
     }
-
     @Test
     fun `when the api doesnt return anything then get values from database`() = runBlocking {
         //Given
         coEvery { quoteRepository.getAllQuotesFromApi() } returns emptyList()
-
         //When
         getQuotesUseCase()
-
         //Then
         coVerify(exactly = 1) { quoteRepository.getAllQuotesFromDatabase() }
     }
 
-    @Test
-    fun `when the api return something then get values from api`() = runBlocking {
-        //Given
-        val myList = listOf(Quote("Déjame un comentario", "AristiDevs"))
-        coEvery { quoteRepository.getAllQuotesFromApi() } returns myList
-
-        //When
-        val response = getQuotesUseCase()
-
-        //Then
-        coVerify(exactly = 1) { quoteRepository.clearQuotes() }
-        coVerify(exactly = 1) { quoteRepository.insertQuotes(any()) }
-        coVerify(exactly = 0) { quoteRepository.getAllQuotesFromDatabase() }
-        assert(response == myList)
-    }
 }
